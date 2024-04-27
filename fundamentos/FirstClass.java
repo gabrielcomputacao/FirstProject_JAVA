b6c2e619b23a1ceeb2a6d054daa8b35c96466cae
gabriel <gabrielcomputacao@outlook.com>
2024-04-27 07:27:37 -0300

    feat: first class

---
+++ fundamentos/FirstClass.java
@@ -0,0 +1,23 @@
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        // Numeros => (int, float,double,long)
+        // Texto => (String)
+        // Booleano => (boolean)
+
+        int dadoDoTipoInt = 10;
+        // double é o tipo padrao do java em numeros fracionários
+        double dadoDoTipoDouble = 3.14;
+        // precisa colocar o f no final de dados floats
+        float dadoDoTipoFloat = 3.155f;
+        // colocar o L no final para long
+        long dadoDoTipoLong = 21312312321432423L;
+
+        String texto = "texto";
+
+        boolean dadoDoTipoBoolean = false;
+    }
+
+}
